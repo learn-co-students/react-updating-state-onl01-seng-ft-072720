ecbf701cbb077ae8d22a123fd7f72b59711e3ef6
mpmoran <kamokumoran@gmail.com>
2020-11-30 16:00:05 -0800

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,43 @@
-// Code ClickityClick Component Here
+import React from 'react';
+
+class ClickityClick extends React.Component {
+    constructor() {
+        super();
+
+        // Define the initial state:
+        this.state = {
+            hasBeenClicked: false
+        };
+    }
+
+    handleClick = () => {
+        this.setState({
+            hasBeenClicked: true
+            // addressInfo: {
+            //     ...this.state.addressInfo,
+            //     city: 'New York City'
+            // }
+        })
+    };
+
+    // handleClick = () => {
+    //     this.setState(previousState => {
+    //         return {
+    //             toggled: !previousState.toggled
+    //         }
+    //     })
+    // }
+
+    render() {
+        return (
+            <div>
+                <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+                <button onClick={this.handleClick}>Click me!</button>
+            </div>
+        );
+    }
+}
+
+export default ClickityClick;
+
+// src/index.js
